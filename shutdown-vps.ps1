c427bfc11ee3378fd32e57cf1a4f61eeb7975723
HoangThanhTung <90949869+tung2k1@users.noreply.github.com>
2025-05-31 11:25:41 +0700

    Update shutdown-vps.ps1

--- shutdown-vps.ps1
+++ shutdown-vps.ps1
@@ -12,7 +12,7 @@ $vps_groups = @{
     );
     "Tat_Ca_VPS" = @(
         "103.253.21.231",
-        "103.253.21.231"
+        "103.253.21.156"
         # Dam bao tat ca cac IP/Hostname tu cac nhom khac duoc them vao day neu ban muon co lua chon tat ca
     );
     # Them cac nhom khac cua ban vao day theo cu phap "TenNhom" = @("IP1", "IP2", "Hostname3");
